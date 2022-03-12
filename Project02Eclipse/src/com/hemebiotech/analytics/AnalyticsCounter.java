d0afcf9083012e4d8b1781419abb339be83aa8d9
TATA-YOYO <4learnhacking@free.fr>
2022-03-12 12:19:44 +0100

    Adaptation of the "analyticsCounter" class to the new "Symptoms" class

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,37 +1,19 @@
 package com.hemebiotech.analytics;
 
 import java.io.*;
-import java.util.HashMap;
 import java.util.Map;
 
 public class AnalyticsCounter {
     public static void main(String[] args) throws Exception {
         // first get input
-        Map<String, Integer> mapOfSymptoms = new HashMap<String, Integer>();
-        BufferedReader reader = new BufferedReader(new FileReader(args[0]));
-        String line = reader.readLine();
-        mapOfSymptoms.put(line,1);
-        line = reader.readLine();
-
-
-        while (line != null) {
-
-            if (mapOfSymptoms.get(line)==null){
-                mapOfSymptoms.put(line,1);
-            }
-            else{
-                int v = mapOfSymptoms.get(line);
-                v++;
-                mapOfSymptoms.put(line,v);
-            }
-            line = reader.readLine();
-        }
+        ReaderOfSymptoms myReaderOfSymptoms = new ReaderOfSymptoms();
+        Symptoms mySymptoms = new Symptoms(myReaderOfSymptoms, "C:\\Users\\Marc-black\\s.txt");
 
         // next generate output
         BufferedWriter writer = new BufferedWriter (new FileWriter("C:\\Users\\Marc-black\\Desktop\\result.txt"));
 
         try {
-            for (Map.Entry<String, Integer> entre : mapOfSymptoms.entrySet()) {
+            for (Map.Entry<String, Integer> entre : mySymptoms.mapOfSymptoms.entrySet()) {
                 writer.write(entre.getKey() + " :" + entre.getValue());
                 writer.newLine();
             }
